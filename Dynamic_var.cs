aff71fad02e833187a741c5b35e57a5c6bdff224
Mohanapriya2828 <mohanapriya.jayabalan@psiog.com>
2025-10-02 18:46:48 +0530

    Dynamic_var example commit

---
+++ Dynamic_var.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        // a = "hello"; 
+
+        dynamic b = 10;
+        Console.WriteLine("b as int: " + b);
+
+        b = "hello";  
+        Console.WriteLine("b as string: " + b);
+        // int result = b + 5; 
+    }
+}
